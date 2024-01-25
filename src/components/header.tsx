9ac2c3d2300820afe0fc761bcd70eaefc613afd0
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2024-01-25 14:06:14 +0100

    add link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,30 +2,33 @@ import Image from "next/image";
 import euLight from "../../public/eu-light.png";
 import euDark from "../../public/eu-dark.png";
 import ModeToggle from "./mode-toggle";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header>
-      <div className="flex min-w-max items-center justify-between p-4">
-        <div className="flex items-center">
-          <Image
-            className="hidden dark:block"
-            src={euLight}
-            priority
-            height={40}
-            width={40}
-            alt="eu stars"
-          />
-          <Image
-            className="block dark:hidden"
-            src={euDark}
-            priority
-            height={40}
-            width={40}
-            alt="eu stars"
-          />
-          <h1 className="font-bold">wheretomove.eu</h1>
-        </div>
+      <div className="flex min-w-max cursor-pointer items-center justify-between p-4">
+        <Link href="/">
+          <div className="flex items-center">
+            <Image
+              className="hidden dark:block"
+              src={euLight}
+              priority
+              height={40}
+              width={40}
+              alt="eu stars"
+            />
+            <Image
+              className="block dark:hidden"
+              src={euDark}
+              priority
+              height={40}
+              width={40}
+              alt="eu stars"
+            />
+            <h1 className="font-bold">wheretomove.eu</h1>
+          </div>
+        </Link>
         <ModeToggle />
       </div>
     </header>
