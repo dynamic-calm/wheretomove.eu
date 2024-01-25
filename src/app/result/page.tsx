1d9d8c82824e7e68e09908533e3435bf07f929e8
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2024-01-25 23:22:23 +0100

    simplify

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,10 +1,9 @@
+import { COUNTRIES } from "@/config";
 import JSONstat from "jsonstat-toolkit";
 
 export default async function ResultPage({
-  params,
   searchParams,
 }: {
-  params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const items = JSON.parse(searchParams.items as string);
@@ -30,8 +29,8 @@ async function getData(id: string) {
   const url =
     "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/ilc_di03?format=JSON&unit=EUR&sex=T&indic_il=MED_E&age=Y18-64&lang=en";
 
-  const jsonStat = await JSONstat(url);
-  const ds = jsonStat.Dataset(0);
+  const jst = await JSONstat(url);
+  const ds = jst.Dataset(0);
   const filter = {
     age: ["Y18-64"],
     unit: ["EUR"],
@@ -40,29 +39,21 @@ async function getData(id: string) {
     indic_il: ["MED_E"],
   };
 
-  const filtered = ds.Dice(filter, { clone: true });
+  const filtered = ds.Dice(filter);
   const data = filtered.toTable();
   const processedData = data
-    .flatMap((item: Item) =>
-      item.at(-1) ? [{ country: item.at(-3), data: item.at(-1) }] : [],
-    )
-    .sort((a: FlattenedItem, b: FlattenedItem) => b.data - a.data);
+    .flatMap((item: unknown[]) => {
+      const el = item.at(-3);
+      const country = typeof el === "string" ? el : "";
+      const data = item.at(-1) ?? 0;
+      return !COUNTRIES.has(country) || !data ? [] : [{ country, data }];
+    })
+    .sort((a: FItem, b: FItem) => b.data - a.data);
 
   return processedData;
 }
 
-type Item = [
-  "Annual",
-  "From 18 to 64 years",
-  "Total",
-  "Mean equivalised net income",
-  "Euro",
-  string,
-  "2022",
-  number | null,
-];
-
-interface FlattenedItem {
+interface FItem {
   country: string;
   data: number;
 }
