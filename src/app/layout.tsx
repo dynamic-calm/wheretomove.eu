1100b3d46409dfdff3ed3b74d3ac4a3cd6e6f98a
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2024-01-25 13:24:09 +0100

    add theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,11 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} flex min-h-screen flex-col antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-export default function Home({}) {
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex flex-col items-center px-10 text-center md:w-2/3">
-        <h2 className="text-4xl font-semibold">
+        <h2 className="text-4xl font-bold">
           Discover your ideal country based on data-driven decision making.
         </h2>
-        <h3 className="py-5 text-xl">
+        <h3 className="py-5 text-xl font-semibold">
           Embark on a journey of informed choices.
         </h3>
       </div>
+      <Button>Button</Button>
     </main>
   );
 }
