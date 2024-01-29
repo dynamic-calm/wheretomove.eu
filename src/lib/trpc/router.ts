f08cf9f907522de09c120e30ac4af0f2aeb4394c
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2024-01-29 17:47:23 +0100

    fix build

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { IDS } from "@/config";
+import { type Ids } from "@/config";
 import { publicProcedure, router } from "./init";
 import QUERIES from "./queries";
 
@@ -10,7 +10,7 @@ export const appRouter = router({
 });
 
 async function getData(id: string) {
-  const query = QUERIES.get(id as IDS)!;
+  const query = QUERIES.get(id as Ids)!;
   return await query();
 }
 
