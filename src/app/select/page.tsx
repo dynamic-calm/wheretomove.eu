c09793a50c65911a3a8fcf49ffda961989deb02e
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2024-01-29 11:18:26 +0100

    test skeletonm

--- src/app/select/page.tsx
+++ src/app/select/page.tsx
@@ -1,16 +1,30 @@
 "use client";
 
 import { CheckBoxForm } from "@/components/checkbox-form";
+import { Skeleton } from "@/components/ui/skeleton";
 import { CHECKBOX_ITEMS } from "@/config";
+import { Suspense } from "react";
 
 export default function Start() {
+  const fallBack = (
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  );
+
   return (
     <>
-      <CheckBoxForm
-        title="What matters"
-        description="Select what you value the most in a country."
-        items={CHECKBOX_ITEMS}
-      />
+      <Suspense fallback={fallBack}>
+        <CheckBoxForm
+          title="What matters"
+          description="Select what you value the most in a country."
+          items={CHECKBOX_ITEMS}
+        />
+      </Suspense>
     </>
   );
 }
