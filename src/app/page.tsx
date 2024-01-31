cec007d854544f5b87221fbe6c55f96c7fad27f7
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2024-01-31 17:53:17 +0100

    fix header and padding on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
 
   return (
     <div className="flex w-auto max-w-[22.5rem] flex-grow flex-col items-center justify-evenly md:max-w-[40rem]">
-      <div className="pt-auto flex flex-col items-center justify-between gap-2">
+      <div className="pt-auto flex flex-col items-center justify-between gap-2 py-6">
         <h1 className="scroll-m-20 text-center text-4xl font-extrabold  lg:text-5xl">
           Where should I move?
         </h1>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50">
+    <header>
       <div className="mx-auto w-screen max-w-6xl">
         <div className="flex items-center justify-between p-4">
           <Link href="/">
