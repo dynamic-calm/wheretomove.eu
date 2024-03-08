f8133b31299ea9f2a9923c885d9de89d3d745e7d
Mateo Presa Castro <mateopresacastro@gmail.com>
2024-03-08 21:10:01 +0100

    once

--- src/components/motion-div.tsx
+++ src/components/motion-div.tsx
@@ -6,6 +6,7 @@ export default function Mdiv({ children }: { children: React.ReactNode }) {
     <motion.div
       initial={{ opacity: 0, translateY: 0 }}
       whileInView={{ opacity: 1, translateY: -10 }}
+      viewport={{ once: true }}
     >
       {children}
     </motion.div>
